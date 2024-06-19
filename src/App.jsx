73c7f289e67730cf94e44839445733bde51b8241
luy06D <cusiluis04@gmail.com>
2024-06-19 02:23:31 -0500

    personajes en cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useState } from "react"
 import {Input, Button} from "@nextui-org/react"
 import './App.css'
 import {Personajes} from './components/cards_dvz'
 import { getPersonajes } from "./service/personajes"
 import { useSearch } from "./hooks/useSearch"
 import debounce from "just-debounce-it"
-
 
 
 
@@ -46,9 +45,9 @@ function App() {
       <h1 className="mt-4 title">DVZ BUSCADOR POR RAZA</h1>
       <form className="form-search" onSubmit={handleSubmit}>
         <Input onChange={handleChange} value={search} className="mt-3 mb-3" type="text" placeholder="Saiyan , Human, Namekian" />
-        <Button type="submit" color="primary" variant="shadow">Buscar</Button>
+        <Button type="submit" color="warning" variant="shadow">Buscar</Button>
       </form>
-        {error && <p className="mb-3" style={{color: '#DE301D'}}>{error}</p>}
+        {error && <p className="mb-3" style={{color: '#F5A524'}}>{error}</p>}
       </header>
       <main >
         <Personajes responsedbz={response}/>
--- src/components/cards_dvz.jsx
+++ src/components/cards_dvz.jsx
@@ -1,13 +1,29 @@
+import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 
 function ListDvz ({responsedbz}) {
     return(
-        <ul className="cards">{responsedbz.map(response => (
-            <li key={response.id}>
-              <h3>{response.name}</h3>
-              <p>{response.ki}</p>
-              <img src={response.image} alt="" />
-            </li>
-          ))}</ul>
+        <div className="tarjeta gap-2 grid grid-cols-2 sm:grid-cols-4">
+            {responsedbz.map( response => (
+              <Card className="py-4" key={response.id}>
+              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h4 className="font-bold text-large">{response.name}</h4>
+                <small className="text-default-500">Ki: {response.ki}</small>
+                <p className="text-tiny uppercase font-bold"></p>
+              </CardHeader>
+              <CardBody className="overflow-visible py-2">
+                <Image
+                  alt={response.name}
+                  className="object-cover rounded-xl"
+                  src={response.image}
+                  width={270}
+                />
+              </CardBody>
+            </Card>
+            ))}
+
+        </div>
+
+  
     )
 }
 
