906cb36388775f5fb1e2f6604192bffc86947e51
luy06D <cusiluis04@gmail.com>
2024-06-21 23:07:30 -0500

    fetchCaracteristicas correct

--- src/components/modal_dvz.jsx
+++ src/components/modal_dvz.jsx
@@ -1,16 +1,33 @@
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button} from "@nextui-org/react";
-
+import { getCaracteristicas } from "../service/caracteristicas";
+import { useEffect, useState } from "react";
 
 export function ModalDvz ({openModal, closeModal, selectKey}){
+    const [responseCarac, setResponseCarac] = useState(null)
+
+    useEffect(() =>{
+      if(openModal){
+        const fetchCaracteristicas = async () =>{
+          const data = await getCaracteristicas({id: selectKey})
+          console.log(data)
+          setResponseCarac(data)
+        }
+
+        fetchCaracteristicas()
+      }
+
+    }, [openModal, selectKey])
+
 
     return(
       
       <Modal  isOpen={openModal} onClose={closeModal} size="4xl" >
-
+        {responseCarac !== null && (
         <ModalContent>
-              <ModalHeader className="flex flex-col gap-1">Caracteristicas del personaje</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">Caracteristicas de {responseCarac.name}</ModalHeader>
               <ModalBody>
                 <p>La key es : {selectKey}</p>
+                <p>Descripción: {responseCarac.description}</p>
               </ModalBody>
               <ModalFooter>
                 <Button color="danger"  onClick={closeModal}>
@@ -18,6 +35,7 @@ export function ModalDvz ({openModal, closeModal, selectKey}){
                 </Button>
               </ModalFooter>
         </ModalContent>
+        )}
       </Modal>
     )
 
